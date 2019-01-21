a1b3f63e38755023a88be7c9b05669862d4138fd
Youngrok Kim <rok0810@gmail.com>
2019-01-21 13:53:14 +0900

    Add grafana provisioning configuration
    - update grafana to v5
    - update database init sql

--- db/init.sql
+++ db/init.sql
@@ -1,23 +1,6 @@
 CREATE DATABASE closer_event;
 CREATE DATABASE grafana;
 
--- Create a group
-CREATE ROLE readonly_role;
-
--- Grant access to existing tables
-GRANT USAGE ON SCHEMA public TO readonly_role;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_role;
-
--- Grant access to future tables
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly_role;
-
--- Create a grafana user
-CREATE USER grafana_user;
-GRANT readonly_role TO grafana_user;
-
--- Grant all access on the grafana database
-GRANT ALL PRIVILEGES ON DATABASE grafana TO grafana_user;
-
 -- Create events table to the closer_event db
 \c closer_event;
 CREATE TABLE IF NOT EXISTS events (
@@ -28,6 +11,21 @@ CREATE TABLE IF NOT EXISTS events (
     data JSONB DEFAULT '{}'::jsonb,
     timestamp TIMESTAMP WITH TIME ZONE NOT NULL
 );
+CREATE INDEX events_event_source_id ON events (event, source_id);
 
-CREATE INDEX events_event_source_id
-  ON events (event, source_id);
+-- Create user role
+CREATE ROLE readonly_role;
+
+-- Grant access to existing tables
+GRANT USAGE ON SCHEMA public TO readonly_role;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_role;
+
+-- Grant access to future tables
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly_role;
+
+-- Create a grafana user
+CREATE USER grafana_user; --WITH PASSWORD 6r4f4n4_p455w0rd;
+GRANT readonly_role TO grafana_user;
+
+-- Grant all access on the grafana database
+GRANT ALL PRIVILEGES ON DATABASE grafana TO grafana_user;
